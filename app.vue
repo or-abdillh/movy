7f48ed861f0882813c258fd941de7528fa2013d5
or-abdillh <hans.abdillh05@gmail.com>
2025-05-17 11:28:57 +0800

    feat: refactor app.vue to use VitePwaManifest and NuxtPage components

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <VitePwaManifest />
+  <NuxtPage />
 </template>
+
+<script setup lang="ts">
+
+import { useRuntimeConfig } from '#app';
+
+// composable
+const config = useRuntimeConfig();
+
+// default title
+useHead({
+  titleTemplate(title) {
+    return title ? `${config.public.appName} | ${title}` : config.public.appName
+  },
+})
+
+</script>
