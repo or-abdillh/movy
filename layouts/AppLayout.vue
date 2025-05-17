40dd85eead104c7de9391e22b456b8a7e02ffc82
or-abdillh <hans.abdillh05@gmail.com>
2025-05-17 17:13:39 +0800

    feat: create AppLayout component with navbar and bottom bar

---
+++ layouts/AppLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <PartialsNavbar />
+  <main class="min-h-screen bg-gray-50 pt-16 lg:pt-28">
+    <slot />
+  </main>
+  <PartialsBottomBar />
+</template>
+
+
+<script setup lang="ts"></script>
