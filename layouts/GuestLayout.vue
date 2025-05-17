88dba7b7a7b487d82f8a8c945d9a8d04a865dc5b
or-abdillh <hans.abdillh05@gmail.com>
2025-05-17 12:18:10 +0800

    fix: update background color of GuestLayout to gray-50

--- layouts/GuestLayout.vue
+++ layouts/GuestLayout.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="min-h-screen bg-white">
+  <main class="min-h-screen bg-gray-50">
     <!-- gradient ellipse -->
     <span class="size-96 bg-primary-200 block rounded-full blur-2xl fixed top-0 left-0 -translate-y-2/4 -translate-x-1/4"></span>
     <span class="size-96 bg-primary-200 block rounded-full blur-2xl fixed bottom-0 right-0 translate-y-2/4 translate-x-1/4"></span>
