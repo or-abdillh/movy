2698e2d69a07e31e6b12edb4d7af708d350864e6
or-abdillh <hans.abdillh05@gmail.com>
2025-05-17 17:13:21 +0800

    feat: add Mono component with layout and styling

---
+++ components/brands/Mono.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center items-center gap-4">
+    <div>
+      <p class="text-2xl font-bold bg-gradient-to-r from-primary-700 to-secondary-700 bg-clip-text text-transparent">
+        Movy
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
