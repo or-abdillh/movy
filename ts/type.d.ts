d9981a1f1a15f1e89d33a1b19bb18337f8062cc7
or-abdillh <hans.abdillh05@gmail.com>
2025-05-17 14:53:49 +0800

    feat: add StravaTokenExchangeResponse type definition

---
+++ ts/type.d.ts
@@ -0,0 +1,8 @@
+export type StravaTokenExchangeResponse = {
+    token_type: string;
+    expires_at: number;
+    expires_in: number;
+    refresh_token: string;
+    access_token: string;
+    athlete: Athlete
+}
