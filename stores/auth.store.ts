8a3e303a7370dd8e91471c6eddd6ac6e27f995af
or-abdillh <hans.abdillh05@gmail.com>
2025-05-17 14:53:38 +0800

    feat: add auth store for managing athlete credentials and state

---
+++ stores/auth.store.ts
@@ -0,0 +1,42 @@
+import { defineStore } from "pinia";
+import type { Athlete } from "~/schemas/athlete.schema";
+
+type Credentials = {
+    tokenType: string;
+    expiresAt: number;
+    expiresIn: number;
+    refreshToken: string;
+    accessToken: string;
+};
+
+export const useAuthStore = defineStore("auth", () => {
+
+    // states
+    const athlete = ref<Athlete>()
+    const credentials = reactive<Credentials>({
+        tokenType: "",
+        expiresAt: 0,
+        expiresIn: 0,
+        refreshToken: "",
+        accessToken: ""
+    });
+
+    // setter
+    const setAthlete = (payload: Athlete | undefined = undefined) => athlete.value = payload
+    const setCredentials = (payload: Credentials | undefined = undefined) => {
+        if (payload) {
+            credentials.tokenType = payload.tokenType;
+            credentials.expiresAt = payload.expiresAt;
+            credentials.expiresIn = payload.expiresIn;
+            credentials.refreshToken = payload.refreshToken;
+            credentials.accessToken = payload.accessToken;
+        }
+    }
+
+    return {
+        athlete,
+        credentials,
+        setAthlete,
+        setCredentials
+    }
+})
