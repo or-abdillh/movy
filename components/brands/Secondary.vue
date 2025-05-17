5f6efcb616b1a8908a4c8f19c02aab83c71be452
or-abdillh <hans.abdillh05@gmail.com>
2025-05-17 20:55:50 +0800

    feat: create Secondary component with logo and title

---
+++ components/brands/Secondary.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center items-center gap-3">
+    <img class="size-8" src="/icons/100.png" alt="Movy">
+    <p class="text-slate-700 text-2xl font-bold">Movy</p>
+  </div>
+</template>
+
+<style scoped></style>
