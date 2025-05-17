bbc08f49948834064701ed5a722d6781915c64f2
or-abdillh <hans.abdillh05@gmail.com>
2025-05-17 12:17:51 +0800

    feat: add Primary component for brand display

---
+++ components/Brands/Primary.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center items-center gap-4">
+    <img class="size-16" src="/icons/100.png" alt="Movy">
+    <div>
+      <p class="text-slate-700 text-2xl font-bold">Movy</p>
+      <p class="text-slate-500 text-lg">Move and Story</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
