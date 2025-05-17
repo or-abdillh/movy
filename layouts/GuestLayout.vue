2ce04b5ba3ab147337d914ba039b93e7b93ba528
or-abdillh <hans.abdillh05@gmail.com>
2025-05-17 11:29:10 +0800

    feat: add GuestLayout component with gradient background and slot for content

---
+++ layouts/GuestLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="min-h-screen bg-white">
+    <!-- gradient ellipse -->
+    <span class="size-96 bg-primary-200 block rounded-full blur-2xl fixed top-0 left-0 -translate-y-2/4 -translate-x-1/4"></span>
+    <span class="size-96 bg-primary-200 block rounded-full blur-2xl fixed bottom-0 right-0 translate-y-2/4 translate-x-1/4"></span>
+
+    <!-- content -->
+    <main class="min-h-screen p-8">
+      <slot></slot>
+    </main>
+  </main>
+</template>
+
+<style scoped></style>
