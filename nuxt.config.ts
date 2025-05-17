9608ab5297ecddc74962e05f647054714749463e
or-abdillh <hans.abdillh05@gmail.com>
2025-05-17 11:28:26 +0800

    feat: update nuxt.config.ts to include additional modules and font configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,28 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts']
+  modules: ['@nuxt/fonts', '@vite-pwa/nuxt', '@nuxtjs/tailwindcss'],
+
+  runtimeConfig: {
+    public: {
+      appName: process.env.NUXT_PUBLIC_APP_NAME,
+    }
+  },
+
+  fonts: {
+    families: [
+      {
+        name: "Plus Jakarta Sans",
+        provider: "google",
+        weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        styles: ["normal", "italic"],
+      },
+      {
+        name: "Inter",
+        provider: "google",
+        weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        styles: ["normal", "italic"],
+      }
+    ]
+  }
 })
