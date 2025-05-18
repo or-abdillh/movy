ab2c28e6f22f1254255c3929a67c9ef6dee4e456
or-abdillh <hans.abdillh05@gmail.com>
2025-05-18 20:17:23 +0800

    feat: create Primary button component with styling and slot support

---
+++ components/buttons/Primary.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    class="bg-primary-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md outline outline-transparent hover:outline-primary-400 outline-offset-2 transition duration-200 ease-in-out">
+    <slot />
+  </button>
+</template>
