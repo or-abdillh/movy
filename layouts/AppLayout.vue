2db3594eb8dfdb26e09d7f51d668b56b7a86f4b7
or-abdillh <hans.abdillh05@gmail.com>
2025-05-18 21:39:37 +0800

    fix: adjust padding in main layout for improved spacing

--- layouts/AppLayout.vue
+++ layouts/AppLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <PartialsNavbar />
-  <main class="min-h-screen bg-gray-50 py-20 lg:py-28">
+  <main class="min-h-screen bg-gray-50 pt-16 pb-32 lg:py-28">
     <slot />
   </main>
   <PartialsBottomBar />
