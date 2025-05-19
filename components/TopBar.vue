28433f7254dc44dba1a50a7f0c6bc57f61e2a9a5
or-abdillh <hans.abdillh05@gmail.com>
2025-05-19 21:49:28 +0800

    feat: add TopBar component with back navigation and title display

---
+++ components/TopBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <ResponsiveContainer class="flex justify-between items-center py-4 px-5">
+    <!-- back -->
+    <button @click="$router.back()" class="text-gray-700">
+      <i class="fa-solid fa-arrow-left-long"></i>
+    </button>
+
+    <p class="text-lg font-semibold text-slate-700">{{ props.title }}</p>
+
+    <button></button>
+  </ResponsiveContainer>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  title: string
+}
+
+// props
+const props = defineProps<Props>()
+
+</script>
