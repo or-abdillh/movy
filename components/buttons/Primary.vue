0669116c2293900eb1c0b9ad19c5f35a220a709a
or-abdillh <hans.abdillh05@gmail.com>
2025-05-29 20:30:14 +0800

    fix: remove redundant border width from outline variant in Primary button

--- components/buttons/Primary.vue
+++ components/buttons/Primary.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 
 const classes = computed(() => {
   if (props.variant === 'outline') {
-    return 'bg-transparent border-2 border-primary-600 text-primary-600 font-semibold py-2 px-4 rounded-2xl outline outline-transparent hover:outline-primary-400 outline-offset-2 transition duration-200 ease-in-out'
+    return 'bg-transparent border border-primary-600 text-primary-600 font-semibold py-2 px-4 rounded-2xl outline outline-transparent hover:outline-primary-400 outline-offset-2 transition duration-200 ease-in-out'
   }
   // default: solid
   return 'bg-primary-600 text-white font-semibold py-2 px-4 rounded-2xl outline outline-transparent hover:outline-primary-400 outline-offset-2 transition duration-200 ease-in-out'
