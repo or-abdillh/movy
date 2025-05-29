274e70605ca423590909a3f27630b59c322bb548
or-abdillh <hans.abdillh05@gmail.com>
2025-05-29 14:58:37 +0800

    feat: add Picker component for dynamic skin preview based on card slug

---
+++ components/Skins/Picker.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- preview card -->
+    <section class="w-full">
+      <component :is="skins[card.slug]" />
+    </section>
+</template>
+
+<script setup lang="ts">
+
+import type { Card } from '~/ts/type';
+import {
+  SkinsClassicCleanSunsetVita
+} from "#components";
+import type { DefineComponent } from 'vue';
+
+interface Props {
+  card: Card
+}
+
+// Props
+const { card } = defineProps<Props>();
+
+// components
+const skins: Record<string, DefineComponent<{}, {}, {}, {}, {}>> = {
+  "sunset-vista": SkinsClassicCleanSunsetVita,
+}
+
+</script>
