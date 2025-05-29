1df24954fea7534b6f34b8a798c5970fe74502a6
or-abdillh <hans.abdillh05@gmail.com>
2025-05-29 14:59:23 +0800

    feat: add MadeWithPrimary component for branding display

---
+++ components/Brands/MadeWithPrimary.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-between items-center gap-4">
+    <p class="text-slate-100 text-sm">
+      Made With <span class="font-bold bg-gradient-to-r from-primary-600 to-secondary-900 bg-clip-text text-transparent">Movy</span>
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
