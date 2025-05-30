e12fa5a5bbb723af8b78096923d1af423c5590ed
or-abdillh <hans.abdillh05@gmail.com>
2025-05-30 21:09:17 +0800

    feat: add Text component for input with label and placeholder support

---
+++ components/inputs/Text.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <label v-if="label" class="block mb-2 text-sm font-semibold text-slate-900 capitalize">{{ label }}</label>
+    <input type="text" v-model="model"
+      class="bg-slate-100/50 backdrop-blur-sm border border-slate-500 text-slate-900 focus:outline-slate-600 text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full px-2.5 py-3"
+      :placeholder :required />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+interface Props {
+  label?: string;
+  placeholder?: string;
+  required?: boolean;
+}
+
+// Props
+const { label, placeholder = "Placeholder here", required = true } = defineProps<Props>()
+
+// model
+const model = defineModel({
+  type: String,
+  default: ''
+})
+
+</script>
