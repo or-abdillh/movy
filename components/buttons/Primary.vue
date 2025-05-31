772f44ae06c6ab674a239863e4756a96fa2516af
or-abdillh <hans.abdillh05@gmail.com>
2025-05-31 21:17:46 +0800

    fix: correct button border radius in Primary component for outline and solid variants

--- components/buttons/Primary.vue
+++ components/buttons/Primary.vue
@@ -7,10 +7,10 @@ const props = defineProps<{
 
 const classes = computed(() => {
   if (props.variant === 'outline') {
-    return 'bg-transparent border border-primary-600 text-primary-600 font-semibold py-2 px-4 rounded-2xl outline outline-transparent hover:outline-primary-400 outline-offset-2 transition duration-200 ease-in-out'
+    return 'bg-transparent border border-primary-600 text-primary-600 font-semibold py-2 px-4 rounded-xl outline outline-transparent hover:outline-primary-400 outline-offset-2 transition duration-200 ease-in-out'
   }
   // default: solid
-  return 'bg-primary-600 text-white font-semibold py-2 px-4 rounded-2xl outline outline-transparent hover:outline-primary-400 outline-offset-2 transition duration-200 ease-in-out'
+  return 'bg-primary-600 text-white font-semibold py-2 px-4 rounded-xl outline outline-transparent hover:outline-primary-400 outline-offset-2 transition duration-200 ease-in-out'
 })
 </script>
 
