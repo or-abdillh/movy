ba5767acf8a097a6f4bf94f858899d77d90b0beb
or-abdillh <hans.abdillh05@gmail.com>
2025-05-31 21:18:20 +0800

    fix: remove unused read_write_token from Vercel configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,6 @@ export default defineNuxtConfig({
       },
       vercel: {
         blob: {
-          read_write_token: process.env.NUXT_PUBLIC_BLOB_READ_WRITE_TOKEN,
           endpoint: process.env.NUXT_PUBLIC_VERCEL_BLOB_ENDPOINT,
         }
       },
